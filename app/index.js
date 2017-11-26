7400eba33bc3d40c7eaa287621f2574686c48aeb
Srikanth Vinukonda <srikanthchandu.v@gmail.com>
2017-11-26 16:08:44 -0600

    initializinf the project

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import css from './style.css';
+import App from './components/App.js';
+
+
+ReactDOM.render(<App/>, document.getElementById('app'))
